629e305e3c5e2f95ebfd5eaaf440656691486c4f
CodeAritraDhank <aridhank21205@gmail.com>
2024-09-26 09:26:10 +0530

    LL

--- c/LL/Basic.c
+++ c/LL/Basic.c
@@ -15,23 +15,31 @@ struct node* createNode(int val){
 
 struct node* insertAtBegin(struct node* head,int val){
     struct node* newNode = createNode(val);
+
+     if(head == NULL) return newNode;
+
     newNode->next = head;
     return newNode;
 }
 
 struct node* insertAtEnd(struct node* head,int val){
     struct node* newNode = createNode(val);
+
+    if(head == NULL) return newNode;
+
     struct node* curr = head;
     while(curr->next!=NULL){
         curr = curr->next;
     }
     curr->next = newNode;
-    newNode->next = NULL;
     return head;
 }
 
 struct node* insertAtPos(struct node* head,int pos,int val){
     struct node* newnode = createNode(val);
+
+    if(head == NULL) return newnode;
+
     struct node* curr = head;
     for(int i=1;i<pos-1;i++){
         curr = curr->next;
@@ -42,51 +50,129 @@ struct node* insertAtPos(struct node* head,int pos,int val){
 }
 
 struct node* deleteFromBegin(struct node* head){
-    return head->next;
+    if(head == NULL){
+        return head;
+    } 
+
+    struct node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
 }
 
 struct node* deleteFromEnd(struct node* head){
+    if(head == NULL){
+        return head;
+    } 
+
+    if(head->next == NULL){
+        free(head);
+        head = NULL;
+        return head;
+    }
+
     struct node* curr = head;
     while(curr->next->next!=NULL){
         curr = curr->next;
     }
+
+    struct node* temp = curr->next;
     curr->next = NULL;
+    free(temp);
     return head;
 }
 
 struct node* deleteFromPos(struct node* head,int pos){
+    if(head == NULL){
+        return head;
+    } 
+
+     if(head->next == NULL){
+        free(head);
+        head = NULL;
+        return head;
+    }
+
     struct node* curr = head;
     for(int i=1;i<pos-1;i++){
         curr = curr->next;
     }
+    struct node* temp = curr->next;
     curr->next = curr->next->next;
+    free(temp);
     return head;
 }
 
 void printLL(struct node* head){
+    if(head == NULL){
+        printf("Underflow");
+        return;
+    } 
+
     struct node* curr = head;
     while(curr!=NULL){
-        printf("%d ",curr->data);
+        printf("%d -> ",curr->data);
         curr= curr->next;
     }
-    printf("\n");
+    printf("NULL\n");
 }
 
 
 int main(){
-    struct node* head = createNode(1);
-    head = insertAtBegin(head,2);
-    printLL(head);
-    head = insertAtEnd(head,4);
-    printLL(head);
-    head = insertAtPos(head,2,9);
-    printLL(head);
-    // head = deleteFromBegin(head);
-    // printLL(head);
-    // head = deleteFromEnd(head);
-    // printLL(head);
-    head= deleteFromPos(head,3);
-    printLL(head);
+    struct node* head = NULL;
+
+    int choice,val,pos,x=1;
+
+    while(x){
+        printf("\n1. Insert at beginning\n2. Insert at end\n3. Insert at any position\n4. Delete from beginning\n5. Delete from end\n6. Delete from any position\n7. Exit\nEnter your choice : ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter the value : ");
+                scanf("%d",&val);
+                head = insertAtBegin(head,val);
+                printLL(head);
+                break;
+            case 2:
+                printf("Enter the value : ");
+                scanf("%d",&val);
+                head = insertAtEnd(head,val);
+                printLL(head);
+                break;
+            case 3:
+                printf("Enter the position : ");
+                scanf("%d",&pos);
+                printf("Enter the value : ");
+                scanf("%d",&val);
+                head = insertAtPos(head,pos,val);
+                printLL(head);
+                break;
+            case 4:
+                head = deleteFromBegin(head);
+                printLL(head);
+                break;
+            case 5:
+                head = deleteFromEnd(head);
+                printLL(head);
+                break;
+            case 6:
+                if(head!=NULL){
+                    printf("Enter the position : ");
+                    scanf("%d",&pos);
+                    head = deleteFromPos(head,pos);
+                    printLL(head);
+                }
+                else{
+                    printLL(head);
+                }
+                break;
+            case 7:
+                x=0;
+                break;
+            default:
+                printf("Wrong Input");
+        }
+    }
 
     return 0;
 }
